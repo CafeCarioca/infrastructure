9a7ef12904ebf14122c8aaca251023637b5c4e03
Joaco Torres <joaquintorresfripp@hotmail.com>
2024-10-21 22:21:00 -0300

    add bucket versioning

--- modules/s3/s3_bucket.tf
+++ modules/s3/s3_bucket.tf
@@ -1,11 +1,13 @@
 resource "aws_s3_bucket" "lambda_bucket" {
-  bucket        = "carioca-lambda-code-bucket"
-  force_destroy = true  # Para forzar la eliminación del bucket y su contenido en caso de que se destruya
-  versioning {
-    enabled = true  # Activa la versioning en el bucket para mantener diferentes versiones del código
-  }
+  bucket        = "my-lambda-code-bucket"
+  force_destroy = true
 }
 
-output "lambda_bucket_name" {
-  value = aws_s3_bucket.lambda_bucket.bucket
+# Configurar versioning utilizando aws_s3_bucket_versioning
+resource "aws_s3_bucket_versioning" "lambda_bucket_versioning" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
 }
