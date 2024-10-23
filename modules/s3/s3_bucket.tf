8aa36726a09e91a28f8282997975afdd4679ec21
Joaco Casa <joaquintorres@studio4d.com>
2024-10-23 20:00:43 -0300

    add frontend bucket

--- modules/s3/s3_bucket.tf
+++ modules/s3/s3_bucket.tf
@@ -1,3 +1,5 @@
+# Bucket para codigo de las Lambdas
+
 resource "aws_s3_bucket" "lambda_bucket" {
   bucket        = "carioca-lambda-code-bucket"
   force_destroy = true
@@ -25,6 +27,46 @@ resource "aws_s3_bucket_policy" "lambda_bucket_policy" {
         }
         Action   = "s3:GetObject"
         Resource = "arn:aws:s3:::carioca-lambda-code-bucket/*"
+      }
+    ]
+  })
+}
+
+
+# Bucket del frontend
+resource "aws_s3_bucket" "carioca_front" {
+  bucket        = "carioca-front-bucket"
+  force_destroy = true
+
+  website {
+    index_document = "index.html"  # Documento de inicio de tu sitio
+    error_document = "error.html"  # Documento de error para manejar errores
+  }
+}
+
+
+resource "aws_s3_bucket_versioning" "carioca_front_versioning" {
+  bucket = aws_s3_bucket.carioca_front.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_policy" "carioca_front_policy" {
+  bucket = aws_s3_bucket.carioca_front.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "Policy1729635311597"
+    Statement = [
+      {
+        Sid      = "Stmt1729635307903"
+        Effect   = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::892205733758:role/LabRole"
+        }
+        Action   = "s3:GetObject"
+        Resource = "arn:aws:s3:::carioca-front-bucket/*"
       }
     ]
   })
