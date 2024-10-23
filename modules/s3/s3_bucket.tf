87c3bca3eaaef05e7f8bef2a75e84477f454d168
Joaco Casa <joaquintorres@studio4d.com>
2024-10-23 17:45:10 -0300

    Adding bucket policy

--- modules/s3/s3_bucket.tf
+++ modules/s3/s3_bucket.tf
@@ -1,14 +1,31 @@
 resource "aws_s3_bucket" "lambda_bucket" {
-  bucket        = "carioca-lambda-code-bucket"  # Nombre del bucket S3
+  bucket        = "carioca-lambda-code-bucket"
   force_destroy = true
 }
 
 resource "aws_s3_bucket_versioning" "lambda_bucket_versioning" {
-  bucket = aws_s3_bucket.lambda_bucket.id  # Referencia correcta al bucket S3
+  bucket = aws_s3_bucket.lambda_bucket.id
   versioning_configuration {
     status = "Enabled"
   }
 }
 
+resource "aws_s3_bucket_policy" "lambda_bucket_policy" {
+  bucket = aws_s3_bucket.lambda_bucket.id
 
-# Falta agregarle una policy para que la lambda pueda leer
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "Policy1729635311597"
+    Statement = [
+      {
+        Sid      = "Stmt1729635307903"
+        Effect   = "Allow"
+        Principal = {
+          AWS = "arn:aws:iam::892205733758:role/LabRole"
+        }
+        Action   = "s3:GetObject"
+        Resource = "arn:aws:s3:::carioca-lambda-code-bucket/*"
+      }
+    ]
+  })
+}
