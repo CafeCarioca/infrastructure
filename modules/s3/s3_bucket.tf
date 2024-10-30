0367f1c238a8d9e7dc491cd30f67792a4a0d5726
Joaco Casa <joaquintorres@studio4d.com>
2024-10-30 00:04:43 -0300

    comento todo para prueba

--- modules/s3/s3_bucket.tf
+++ modules/s3/s3_bucket.tf
@@ -1,85 +1,85 @@
-# Bucket para codigo de las Lambdas
+# # Bucket para codigo de las Lambdas
 
-resource "aws_s3_bucket" "lambda_bucket" {
-  bucket        = "carioca-lambda-code-bucket"
-  force_destroy = true
-}
+# resource "aws_s3_bucket" "lambda_bucket" {
+#   bucket        = "carioca-lambda-code-bucket"
+#   force_destroy = true
+# }
 
-resource "aws_s3_bucket_versioning" "lambda_bucket_versioning" {
-  bucket = aws_s3_bucket.lambda_bucket.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+# resource "aws_s3_bucket_versioning" "lambda_bucket_versioning" {
+#   bucket = aws_s3_bucket.lambda_bucket.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
 
-resource "aws_s3_bucket_policy" "lambda_bucket_policy" {
-  bucket = aws_s3_bucket.lambda_bucket.id
+# resource "aws_s3_bucket_policy" "lambda_bucket_policy" {
+#   bucket = aws_s3_bucket.lambda_bucket.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Id      = "Policy1729635311597"
-    Statement = [
-      {
-        Sid      = "Stmt1729635307903"
-        Effect   = "Allow"
-        Principal = {
-          AWS = "arn:aws:iam::892205733758:role/LabRole"
-        }
-        Action   = "s3:GetObject"
-        Resource = "arn:aws:s3:::carioca-lambda-code-bucket/*"
-      }
-    ]
-  })
-}
-
-
-# Bucket del frontend
-resource "aws_s3_bucket" "carioca_front" {
-  bucket        = "carioca-front-bucket"
-  force_destroy = true
-
-  website {
-    index_document = "index.html"  # Documento de inicio de tu sitio
-    error_document = "error.html"  # Documento de error para manejar errores
-  }
-}
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Id      = "Policy1729635311597"
+#     Statement = [
+#       {
+#         Sid      = "Stmt1729635307903"
+#         Effect   = "Allow"
+#         Principal = {
+#           AWS = "arn:aws:iam::892205733758:role/LabRole"
+#         }
+#         Action   = "s3:GetObject"
+#         Resource = "arn:aws:s3:::carioca-lambda-code-bucket/*"
+#       }
+#     ]
+#   })
+# }
 
 
-resource "aws_s3_bucket_versioning" "carioca_front_versioning" {
-  bucket = aws_s3_bucket.carioca_front.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
+# # Bucket del frontend
+# resource "aws_s3_bucket" "carioca_front" {
+#   bucket        = "carioca-front-bucket"
+#   force_destroy = true
+
+#   website {
+#     index_document = "index.html"  # Documento de inicio de tu sitio
+#     error_document = "error.html"  # Documento de error para manejar errores
+#   }
+# }
 
 
-# Desactivar "Block All Public Access" en el bucket
-resource "aws_s3_bucket_public_access_block" "carioca_front_public_access_block" {
-  bucket = aws_s3_bucket.carioca_front.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
+# resource "aws_s3_bucket_versioning" "carioca_front_versioning" {
+#   bucket = aws_s3_bucket.carioca_front.id
+#   versioning_configuration {
+#     status = "Enabled"
+#   }
+# }
 
 
-resource "aws_s3_bucket_policy" "carioca_front_policy" {
-  bucket = aws_s3_bucket.carioca_front.id
+# # Desactivar "Block All Public Access" en el bucket
+# resource "aws_s3_bucket_public_access_block" "carioca_front_public_access_block" {
+#   bucket = aws_s3_bucket.carioca_front.id
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Id      = "Policy1729635311597"
-    Statement = [
-      {
-        Sid      = "AddPerm"
-        Effect   = "Allow"
-        Principal = {
-          "AWS": "*"
-        }
-        Action   = "s3:GetObject"
-        Resource = "arn:aws:s3:::carioca-front-bucket/*"
-      }
-    ]
-  })
-}
+#   block_public_acls       = false
+#   block_public_policy     = false
+#   ignore_public_acls      = false
+#   restrict_public_buckets = false
+# }
+
+
+# resource "aws_s3_bucket_policy" "carioca_front_policy" {
+#   bucket = aws_s3_bucket.carioca_front.id
+
+#   policy = jsonencode({
+#     Version = "2012-10-17"
+#     Id      = "Policy1729635311597"
+#     Statement = [
+#       {
+#         Sid      = "AddPerm"
+#         Effect   = "Allow"
+#         Principal = {
+#           "AWS": "*"
+#         }
+#         Action   = "s3:GetObject"
+#         Resource = "arn:aws:s3:::carioca-front-bucket/*"
+#       }
+#     ]
+#   })
+# }
--- modules/sns/sns.tf
+++ modules/sns/sns.tf
@@ -1,9 +1,9 @@
-resource "aws_sns_topic" "carioca_order_updates" {
-  name = var.sns_topic_name
-}
+# resource "aws_sns_topic" "carioca_order_updates" {
+#   name = var.sns_topic_name
+# }
 
-resource "aws_sns_topic_subscription" "email_notification" {
-  topic_arn = aws_sns_topic.carioca_order_updates.arn
-  protocol  = "email"
-  endpoint  = "user@example.com"  # Cambia esto por la dirección de correo electrónico deseada
-}
+# resource "aws_sns_topic_subscription" "email_notification" {
+#   topic_arn = aws_sns_topic.carioca_order_updates.arn
+#   protocol  = "email"
+#   endpoint  = "user@example.com"  # Cambia esto por la dirección de correo electrónico deseada
+# }
