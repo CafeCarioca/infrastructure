5e4eb9eedfca27a0e89091143ca72cb9080ed8d3
Enz1n <enzodeleonb49@gmail.com>
2024-11-02 15:11:12 -0300

    main hasta ahora

--- main.tf
+++ main.tf
@@ -1,30 +1,90 @@
+# Proveedor de AWS
 provider "aws" {
   region = "us-east-1"
 }
 
+# Generador de sufijos únicos
+resource "random_id" "carioca_front_suffix" {
+  byte_length = 4
+}
+
+resource "random_id" "carioca_backoffice_suffix" {
+  byte_length = 4
+}
+
+resource "random_id" "lambda_bucket" {
+  byte_length = 4
+}
+
+# Definir el rol IAM para Lambda
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "LabRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
+
+# Política para permitir el acceso al bucket S3
+resource "aws_iam_policy" "s3_access_policy" {
+  name        = "LambdaS3AccessPolicy"
+  description = "Permite acceso a S3 para funciones Lambda"
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:PutObject"
+        ]
+        Resource = "arn:aws:s3:::carioca-lambda-code-bucket-${random_id.lambda_bucket.hex}/*"
+      }
+    ]
+  })
+}
+
+# Adjuntar la política al rol
+resource "aws_iam_role_policy_attachment" "attach_s3_policy" {
+  policy_arn = aws_iam_policy.s3_access_policy.arn
+  role       = aws_iam_role.lambda_exec_role.name
+}
+
+# VPC--------------------------------------------------------------------------------
 resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"
-  
+
   tags = {
     Name = "MyVPC"
   }
 }
 
+# Subnet--------------------------------------------------------------------------------
 resource "aws_subnet" "subnet_a" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.1.0/24"
   availability_zone = "us-east-1a"
-  
+
   tags = {
     Name = "MySubnetA"
   }
 }
 
 resource "aws_subnet" "subnet_b" {
-  vpc_id     = aws_vpc.my_vpc.id
-  cidr_block = "10.0.2.0/24"
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "10.0.2.0/24"
   availability_zone = "us-east-1b"
-  
+
   tags = {
     Name = "MySubnetB"
   }
@@ -52,25 +112,172 @@ resource "aws_security_group" "rds_sg" {
   }
 }
 
+# RDS--------------------------------------------------------------------------------
 resource "aws_db_subnet_group" "my_db_subnet_group" {
   name       = "my-db-subnet-group"
   subnet_ids = [aws_subnet.subnet_a.id, aws_subnet.subnet_b.id]
 }
 
 resource "aws_db_instance" "default" {
-  allocated_storage       = 10  # Mínimo requerido
-  storage_type            = "standard"  # Almacenamiento magnético estándar
+  allocated_storage       = 10
+  storage_type            = "standard"
   engine                  = "mysql"
   engine_version          = "5.7"
-  instance_class          = "db.t3.micro"  # Clase de instancia económica
+  instance_class          = "db.t3.micro"
   identifier              = "mydb"
   username                = "dbuser"
   password                = "dbpassword"
   vpc_security_group_ids  = [aws_security_group.rds_sg.id]
   db_subnet_group_name    = aws_db_subnet_group.my_db_subnet_group.name
-  backup_retention_period = 1  # Retención de copia de seguridad mínima
-  skip_final_snapshot     = true  # Para evitar costos adicionales al destruir
-  multi_az                = false  # Sin alta disponibilidad
-  performance_insights_enabled = false  # Deshabilitar Performance Insights
-  monitoring_interval     = 0  # Deshabilitar monitoreo avanzado
+  backup_retention_period = 1
+  skip_final_snapshot     = true
+  multi_az                = false
+  performance_insights_enabled = false
+  monitoring_interval     = 0
+}
+
+# Buckets de S3
+# Bucket del frontend
+resource "aws_s3_bucket" "carioca_front" {
+  bucket        = "carioca-front-bucket-${random_id.carioca_front_suffix.hex}"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_website_configuration" "carioca_front_website" {
+  bucket = aws_s3_bucket.carioca_front.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "carioca_front_versioning" {
+  bucket = aws_s3_bucket.carioca_front.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "carioca_front_public_access_block" {
+  bucket = aws_s3_bucket.carioca_front.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "carioca_front_policy" {
+  bucket = aws_s3_bucket.carioca_front.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "Policy1729635311597"
+    Statement = [
+      {
+        Sid      = "AddPerm"
+        Effect   = "Allow"
+        Principal = {
+          "AWS": "*"
+        }
+        Action   = "s3:GetObject"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.carioca_front.bucket}/*"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.carioca_front_public_access_block]
+}
+
+# Bucket del backoffice
+resource "aws_s3_bucket" "carioca_backoffice" {
+  bucket        = "carioca-backoffice-bucket-${random_id.carioca_backoffice_suffix.hex}"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_website_configuration" "carioca_backoffice_website" {
+  bucket = aws_s3_bucket.carioca_backoffice.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "carioca_backoffice_versioning" {
+  bucket = aws_s3_bucket.carioca_backoffice.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "carioca_backoffice_public_access_block" {
+  bucket = aws_s3_bucket.carioca_backoffice.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "carioca_backoffice_policy" {
+  bucket = aws_s3_bucket.carioca_backoffice.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "Policy1729635311599"
+    Statement = [
+      {
+        Sid      = "AddPerm"
+        Effect   = "Allow"
+        Principal = {
+          "AWS": "*"
+        }
+        Action   = "s3:GetObject"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.carioca_backoffice.bucket}/*"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.carioca_backoffice_public_access_block]
+}
+
+# Bucket para código de las Lambdas
+resource "aws_s3_bucket" "lambda_bucket" {
+  bucket        = "carioca-lambda-code-bucket-${random_id.lambda_bucket.hex}"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_versioning" "lambda_bucket_versioning" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_policy" "lambda_bucket_policy" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "Policy1729635311597"
+    Statement = [
+      {
+        Sid      = "Stmt1729635307903"
+        Effect   = "Allow"
+        Principal = {
+          AWS = aws_iam_role.lambda_exec_role.arn
+        }
+        Action   = "s3:GetObject"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.lambda_bucket.bucket}/*"
+      }
+    ]
+  })
 }
--- modules/s3/s3_bucket.tf
+++ modules/s3/s3_bucket.tf
@@ -1,7 +1,136 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Generador de sufijos únicos
+resource "random_id" "carioca_front_suffix" {
+  byte_length = 4
+}
+
+resource "random_id" "carioca_backoffice_suffix" {
+  byte_length = 4
+}
+
+resource "random_id" "lambda_bucket" {
+  byte_length = 4
+}
+
+# Bucket del frontend
+resource "aws_s3_bucket" "carioca_front" {
+  bucket        = "carioca-front-bucket-${random_id.carioca_front_suffix.hex}"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_website_configuration" "carioca_front_website" {
+  bucket = aws_s3_bucket.carioca_front.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "carioca_front_versioning" {
+  bucket = aws_s3_bucket.carioca_front.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "carioca_front_public_access_block" {
+  bucket = aws_s3_bucket.carioca_front.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "carioca_front_policy" {
+  bucket = aws_s3_bucket.carioca_front.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "Policy1729635311597"
+    Statement = [
+      {
+        Sid      = "AddPerm"
+        Effect   = "Allow"
+        Principal = {
+          "AWS": "*"
+        }
+        Action   = "s3:GetObject"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.carioca_front.bucket}/*"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.carioca_front_public_access_block]
+}
+
+# Bucket del backoffice
+resource "aws_s3_bucket" "carioca_backoffice" {
+  bucket        = "carioca-backoffice-bucket-${random_id.carioca_backoffice_suffix.hex}"
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_website_configuration" "carioca_backoffice_website" {
+  bucket = aws_s3_bucket.carioca_backoffice.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "carioca_backoffice_versioning" {
+  bucket = aws_s3_bucket.carioca_backoffice.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "carioca_backoffice_public_access_block" {
+  bucket = aws_s3_bucket.carioca_backoffice.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+resource "aws_s3_bucket_policy" "carioca_backoffice_policy" {
+  bucket = aws_s3_bucket.carioca_backoffice.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Id      = "Policy1729635311599"
+    Statement = [
+      {
+        Sid      = "AddPerm"
+        Effect   = "Allow"
+        Principal = {
+          "AWS": "*"
+        }
+        Action   = "s3:GetObject"
+        Resource = "arn:aws:s3:::${aws_s3_bucket.carioca_backoffice.bucket}/*"
+      }
+    ]
+  })
+
+  depends_on = [aws_s3_bucket_public_access_block.carioca_backoffice_public_access_block]
+}
+
 # Bucket para codigo de las Lambdas
 
 resource "aws_s3_bucket" "lambda_bucket" {
-  bucket        = "carioca-lambda-code-bucket"
+  bucket        = "carioca-lambda-code-bucket-${random_id.lambda_bucket.hex}"
   force_destroy = true
 }
 
@@ -27,58 +156,6 @@ resource "aws_s3_bucket_policy" "lambda_bucket_policy" {
         }
         Action   = "s3:GetObject"
         Resource = "arn:aws:s3:::carioca-lambda-code-bucket/*"
-      }
-    ]
-  })
-}
-
-
-# Bucket del frontend
-resource "aws_s3_bucket" "carioca_front" {
-  bucket        = "carioca-front-bucket"
-  force_destroy = true
-
-  website {
-    index_document = "index.html"  # Documento de inicio de tu sitio
-    error_document = "error.html"  # Documento de error para manejar errores
-  }
-}
-
-
-resource "aws_s3_bucket_versioning" "carioca_front_versioning" {
-  bucket = aws_s3_bucket.carioca_front.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-}
-
-
-# Desactivar "Block All Public Access" en el bucket
-resource "aws_s3_bucket_public_access_block" "carioca_front_public_access_block" {
-  bucket = aws_s3_bucket.carioca_front.id
-
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
-}
-
-
-resource "aws_s3_bucket_policy" "carioca_front_policy" {
-  bucket = aws_s3_bucket.carioca_front.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Id      = "Policy1729635311597"
-    Statement = [
-      {
-        Sid      = "AddPerm"
-        Effect   = "Allow"
-        Principal = {
-          "AWS": "*"
-        }
-        Action   = "s3:GetObject"
-        Resource = "arn:aws:s3:::carioca-front-bucket/*"
       }
     ]
   })
